76e867297b7387ffdfb02dc61c0dcc5b16d06926
ShubhamAbhayDeshpande <deshpandeaa6@gmail.com>
2024-10-19 09:46:38 +0200

    Added function for linked list prepend operation

--- LinkedList/LinkedList.py
+++ LinkedList/LinkedList.py
@@ -97,7 +97,32 @@ class LinkedList:
             self.tail = None
 
         return temp
+    
+    def LLPrepend(self, val):
+        """
+        This method will add a node at the beginning of the linked list. 
+        
+        The following edge cases should be considered:
+        1. When the linked list is empty
+        2. When the list has one or more elements
+        
+        """
+        # Create a new node
+        new_node = Node(val)
 
+        # Condition where the list is empty
+        if self.head is None or self.length == 0: # In reality either one of these conditions is enough.
+            self.head =  new_node
+            self.tail = new_node
+            # Adding one element in an empty list does not actually change the length of the list.
+            # Hence we are not going to increase "self.length" here. 
+
+        # Condition where the list has one or more elements
+        new_node.next = self.head
+        self.head = new_node
+        self.length += 1
+        return True
+        
     def printLL(self):
         """
         Method to print all elements in linked list
@@ -112,59 +137,25 @@ class LinkedList:
 
 if __name__ == "__main__":
 
-    def check(expect, actual, message):
-        print(message)
-        print("EXPECTED:", expect)
-        print("RETURNED:", actual)
-        print("PASS" if expect == actual else "FAIL", "\n")
+    my_linked_list = LinkedList(2)
+    my_linked_list.LLAppend(3)
 
-    print("\n----- Test: Pop on linked list with one node -----\n")
-    linked_list = LinkedList(1)
-    linked_list.printLL()
-    popped_node = linked_list.LLPop()
-    check(1, popped_node.value, "Value of popped node:")
-    check(None, linked_list.head, "Head of linked list:")
-    check(None, linked_list.tail, "Tail of linked list:")
-    check(0, linked_list.length, "Length of linked list:")
+    print('Before prepend():')
+    print('----------------')
+    print('Head:', my_linked_list.head.value)
+    print('Tail:', my_linked_list.tail.value)
+    print('Length:', my_linked_list.length, '\n')
+    print('Linked List:')
+    my_linked_list.printLL()
 
-    print("\n----- Test: Pop on linked list with multiple nodes -----\n")
-    linked_list = LinkedList(1)
-    linked_list.LLAppend(2)
-    linked_list.LLAppend(3)
-    linked_list.printLL()
-    popped_node = linked_list.LLPop()
-    check(3, popped_node.value, "Value of popped node:")
-    check(1, linked_list.head.value, "Head of linked list:")
-    check(2, linked_list.tail.value, "Tail of linked list:")
-    check(2, linked_list.length, "Length of linked list:")
 
-    print("\n----- Test: Pop on empty linked list -----\n")
-    linked_list = LinkedList(1)
-    linked_list.head = None
-    linked_list.tail = None
-    linked_list.length = 0
-    popped_node = linked_list.LLPop()
-    check(None, popped_node, "Popped node from empty linked list:")
-    check(None, linked_list.head, "Head of linked list:")
-    check(None, linked_list.tail, "Tail of linked list:")
-    check(0, linked_list.length, "Length of linked list:")
+    my_linked_list.LLPrepend(1)
 
-    print("\n----- Test: Pop all -----\n")
-    linked_list = LinkedList(1)
-    linked_list.LLAppend(2)
-    linked_list.printLL()
-    popped_node = linked_list.LLPop()
-    check(2, popped_node.value, "Value of popped node (first pop):")
-    check(1, linked_list.head.value, "Head of linked list (after first pop):")
-    check(1, linked_list.tail.value, "Tail of linked list (after first pop):")
-    check(1, linked_list.length, "Length of linked list (after first pop):")
-    popped_node = linked_list.LLPop()
-    check(1, popped_node.value, "Value of popped node (second pop):")
-    check(None, linked_list.head, "Head of linked list (after second pop):")
-    check(None, linked_list.tail, "Tail of linked list (after second pop):")
-    check(0, linked_list.length, "Length of linked list (after second pop):")
-    popped_node = linked_list.LLPop()
-    check(None, popped_node, "Popped node from empty linked list (third pop):")
-    check(None, linked_list.head, "Head of linked list (after third pop):")
-    check(None, linked_list.tail, "Tail of linked list (after third pop):")
-    check(0, linked_list.length, "Length of linked list (after third pop):")
+
+    print('\n\nAfter prepend():')
+    print('---------------')
+    print('Head:', my_linked_list.head.value)
+    print('Tail:', my_linked_list.tail.value)
+    print('Length:', my_linked_list.length, '\n')
+    print('Linked List:')
+    my_linked_list.printLL()
